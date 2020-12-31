cf947e123265826bc7d48233d12a78460c17ea72
cz495969281@qq <495969281@qq.com>
2020-12-31 23:49:08 +0800

    中间部分-销售额&访问量的柱状图以及排行榜页面

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,24 @@
 <template>
   <div class='home'>
     <top-view></top-view>
+    <sales-view></sales-view>
   </div>
 </template>
 
 <script>
 import TopView from '../components/TopView'
+import SalesView from '../components/SalesView'
 export default {
   name: 'Home',
   components: {
-    TopView
+    TopView,
+    SalesView
   }
 }
 </script>
 <style>
 .home {
   width: 100%;
-  height: 100%;
   padding: 20px;
   background: #eee;
   box-sizing: border-box;
