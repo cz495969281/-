144d7fdadb1e06117c17fd180d265c3d678446c2
cz495969281@qq <495969281@qq.com>
2021-01-21 21:25:53 +0800

    完成水球图和词云图的静态页面

--- src/components/MapView/index.vue
+++ src/components/MapView/index.vue
@@ -3,15 +3,40 @@
         <div class="left">
             <b-map-scatter />
         </div>
-        <div class="right">456</div>
+        <div class="right">
+            <el-card shadow="hover">
+                <template v-slot:header>
+                    <div class="title">用户月同比增长</div>
+                </template>
+                <template>
+                    <div class="chart-wrapper">
+                        <liquid-fill />
+                    </div>
+                </template>
+            </el-card>
+            <el-card shadow="hover">
+            <template v-slot:header>
+                <div class="title">热门搜索</div>
+            </template>
+            <template>
+                <div class="chart-wrapper">
+                    <word-cloud />
+                </div>
+            </template>
+            </el-card>
+        </div>
     </div>
 </template>
 
 <script>
 import BMapScatter from '../BMapScatter'
+import LiquidFill from '../LiquidFill'
+import WordCloud from '../WordCloud'
 export default {
     components: {
-        BMapScatter
+        BMapScatter,
+        LiquidFill,
+        WordCloud
     }
 }
 </script>
@@ -28,6 +53,23 @@ export default {
         .right {
             flex: 1;
             display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            margin-left: 20px;
+            .title {
+                display: flex;
+                align-items: center;
+                height: 60px;
+                box-sizing: border-box;
+                border-bottom: 1px solid #eee;
+                font-size: 14px;
+                font-weight: 500;
+                padding: 0 0 0 20px;
+            }
+            .chart-wrapper {
+                width: 100%;
+                height: 190px;
+            }
         }
     }
 </style>
